379555b74c23f3cee1c2abc9dc30f675b19ee20c
429193-20 <429193-20>
2021-12-14 11:34:09 +0300

    First lab

---
+++ ConsoleApp1/B.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class B:A
+    {
+        protected object name1;
+        protected object name2;
+        public B(object one, object two)
+        {
+            this.name1 = one;
+            this.name2 = two;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C c1 = new C(new A(), new A(), new B(new A(), new A()));
+        }
+    }
+}
